3408b92a5bf0268f71a45cb2214df0c236fe13b9
Mahim Mir <intence.it@gmail.com>
2026-03-25 20:27:43 +0530

    Home section arranged

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,22 +1,34 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
-import ShowcaseSection from "@/components/ShowcaseSection";
-import WhySection from "@/components/WhySection";
 import ClientsSection from "@/components/ClientsSection";
-import TestimonialsSection from "@/components/TestimonialsSection";
 import StatsSection from "@/components/StatsSection";
-import FloatingContact from "@/components/FloatingContact";
+import WhySection from "@/components/WhySection";
+import ServicesSection from "@/components/ServicesSection";
+import ShowcaseSection from "@/components/ShowcaseSection";
+import FoundersSection from "@/components/FoundersSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import VisionMissionSection from "@/components/VisionMissionSection";
+import BlogSection from "@/components/BlogSection";
+import ContactSection from "@/components/ContactSection";
+import FAQSection from "@/components/FAQSection";
 import Footer from "@/components/Footer";
+import FloatingContact from "@/components/FloatingContact";
 
 const Index = () => (
   <div className="scroll-smooth">
     <Navbar />
     <HeroSection />
-    <ShowcaseSection />
-    <WhySection />
-    <StatsSection />
     <ClientsSection />
+    <StatsSection />
+    <WhySection />
+    <ServicesSection />
+    <ShowcaseSection />
+    <FoundersSection />
     <TestimonialsSection />
+    <VisionMissionSection />
+    <BlogSection />
+    <ContactSection />
+    <FAQSection />
     <Footer />
     <FloatingContact />
   </div>
