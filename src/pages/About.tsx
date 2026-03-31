2a72adb578db9a4765541fffab1414fefe79caf5
Mahim Mir <intence.it@gmail.com>
2026-03-31 13:24:16 +0530

    about us changed

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/Navbar";
 import AboutSection from "@/components/AboutSection";
-import WhySection from "@/components/WhySection";
 import Footer from "@/components/Footer";
 
 const About = () => (
@@ -8,7 +7,6 @@ const About = () => (
     <Navbar />
     <main className="flex-1 pt-[70px]">
       <AboutSection />
-      <WhySection />
     </main>
     <Footer />
   </div>
