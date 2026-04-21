5d4a38e546c24ea09719121058d2c57aa584e191
Mahim Mir <intence.it@gmail.com>
2026-04-21 13:05:53 +0530

    Home page and about Page Changed

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/Navbar";
 import AboutSection from "@/components/AboutSection";
+import ClientsSection from "@/components/ClientsSection";
 import Footer from "@/components/Footer";
 
 const About = () => (
   <div className="min-h-screen flex flex-col">
     <Navbar />
-    <main className="flex-1 pt-[70px]">
+    <main className="flex-1">
       <AboutSection />
+      <ClientsSection />
     </main>
     <Footer />
   </div>
